6b138ffc481d8547aa754e62e3de591c7756afdb
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 13:40:54 +0100

    Add a test for pawns moving two spaces initially

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -9,7 +9,8 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) {
         let location = board.findPiece(this)
-        if (this.player === Player.WHITE) {
+
+        if (this.player === Player.WHITE && location !== Square.at(0,0)) {
             return Square.at(location.row + 1, location.col)
         } else {
             return Square.at(location.row - 1, location.col)
