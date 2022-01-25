95c7a808fb2638e15823c964643b1ecacfbc4625
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-01-25 14:54:16 +0000

    Made rooks move

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+import Player from '../player';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +8,18 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        let availableMoves = [];
+        for (let i = 0; i < 8; i++) {
+            if (i !== location.col) {
+            availableMoves.push(Square.at(location.row, i));
+            }
+        }
+        for (let i = 0; i < 8; i++) {
+            if (i !== location.row) {
+            availableMoves.push(Square.at(i, location.col));
+            }
+        }
+        return availableMoves;
     }
 }
