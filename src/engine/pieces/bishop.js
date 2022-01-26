0a2aa84608f74d109d3d83aa36f52e5ba72aced6
Deepashri Dali <Deepashri.Dali@softwire.com>
2022-01-26 12:35:27 +0000

    Completed Bishop Tests

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -11,25 +11,70 @@ export default class Bishop extends Piece {
         let location = board.findPiece(this)
         
         let availableMoves = [];
-        //Top right
-        for (let i = location.row+1, j = location.col+1; i<8, j<8; i++,j++ )
-        {                  
-                    availableMoves.push(Square.at(i, j));
+  //Top right
+  for (let i = location.row+1, j = location.col+1; i <=7 ; i++,j++ )
+  {      
+      if(j<=7)     {
+        availableMoves.push(Square.at(i, j));
+      }       
+            
+  }
+  //Top left
+  for (let i = location.row+1, j = location.col-1; i<=7; i++,j-- )
+  {        
+      if(j>=0)      {
+        availableMoves.push(Square.at(i, j));
+      }    
+        
+  }
+  //Bottom left
+  for (let i = location.row-1, j = location.col-1; i>=0; i--,j-- )
+  {    
+    if(j>=0){
+        availableMoves.push(Square.at(i, j));
+    }
+           
+           
+  }
+  //Bottom right
+      for (let i = location.row-1, j = location.col+1; i>=0; i--,j++) {   
+          
+        if(j<=7){
+            availableMoves.push(Square.at(i, j));
         }
-        //Top left
-        for (let i = location.row+1, j = location.col-1; i<=8, j>=0; i++,j-- )
-        {                  
-                    availableMoves.push(Square.at(i, j));
-        }
-        //Bottom left
-        for (let i = location.row-1, j = location.col-1; i>0, j>0; i--,j-- )
-        {                  
-                    availableMoves.push(Square.at(i, j));
-        }
-        //Bottom right
-            for (let i = location.row-1, j = location.col+1; i === 0 || j === 7; i--,j++) {             
-                    availableMoves.push(Square.at(i, j));
-        }
+              
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+        // //Top right
+        // for (let i = location.row+1, j = location.col+1; i <8, j< 8; i++,j++ )
+        // {                  
+        //             availableMoves.push(Square.at(i, j));
+        // }
+        // //Top left
+        // for (let i = location.row+1, j = location.col-1; i<=8, j>=0; i++,j-- )
+        // {                  
+        //             availableMoves.push(Square.at(i, j));
+        // }
+        // //Bottom left
+        // for (let i = location.row-1, j = location.col-1; i>0, j>0; i--,j-- )
+        // {                  
+        //             availableMoves.push(Square.at(i, j));
+        // }
+        // //Bottom right
+        //     for (let i = location.row-1, j = location.col+1; i === 0 || j === 7; i--,j++) {             
+        //             availableMoves.push(Square.at(i, j));
+        // }
               
         return availableMoves;
     }
--- tests/engine/pieces/bishop.spec.js
+++ tests/engine/pieces/bishop.spec.js
@@ -27,18 +27,24 @@ describe('Bishop', () => {
         
         ];
 
-        // const expectedMoves = [
-        //     // Bottom Left
-        //     Square.at(0, 1), 
-        //     // Bottom Right
-        //     Square.at(0, 3), 
-        //     // Top Left
-        //     Square.at(2, 1), Square.at(3, 0),
-        //     // Top Right
-        //     Square.at(2, 3), Square.at(3, 4), Square.at(4, 5), Square.at(5, 6), Square.at(6, 7)
+        // it('can move diagonally', () => {
+        //     const bishop = new Bishop(Player.WHITE);
+        //     board.setPiece(Square.at(3, 3), bishop);
+        //     // board.setPiece(Square.at(1, 2), bishop);
+        //     const moves = bishop.getAvailableMoves(board);
+    
+        //     const expectedMoves = [
+        //                // Top Right
+        //     Square.at(4, 4), Square.at(5, 5), Square.at(6, 6), Square.at(7, 7),
+        //         //Top Left
+        //         Square.at(4, 2),   Square.at(5, 1),   Square.at(6, 0), 
+        //     // // Bottom Left
+        //     Square.at(2, 2),    Square.at(1, 1),    Square.at(0, 0),
+        //     // // Bottom Right
+        //     Square.at(2, 4),   Square.at(1, 5),   Square.at(0, 6) 
+    
             
         //     ];
-
         moves.should.deep.include.members(expectedMoves);
     });
 
