f438843dad17c3fc4527c41ace9f859f980c2ab7
Deepashri Dali <Deepashri.Dali@softwire.com>
2022-01-26 08:23:17 +0000

    Added tests for King and working on Bishop

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -9,20 +9,21 @@ export default class Bishop extends Piece {
 
     getAvailableMoves(board) {
         let location = board.findPiece(this)
+        
         let availableMoves = [];
-        for (let i = location.row+1, j = location.col+1; i<=8, j<=8; i++,j++ )
+        for (let i = location.row+1, j = location.col+1; i<8, j<8; i++,j++ )
         {                  
                     availableMoves.push(Square.at(i, j));
         }
-        for (let i = location.row+1, j = location.col-1; i<=8, j<=8; i++,j-- )
+        for (let i = location.row+1, j = location.col-1; i<8, j>0; i++,j-- )
         {                  
                     availableMoves.push(Square.at(i, j));
         }
-        for (let i = location.row-1, j = location.col-1; i<=8, j<=8; i--,j-- )
+        for (let i = location.row-1, j = location.col-1; i>0, j>0; i--,j-- )
         {                  
                     availableMoves.push(Square.at(i, j));
         }
-        for (let i = location.row-1, j = location.col+1; i<=8, j<=8; i--,j++ )
+        for (let i = location.row-1, j = location.col+1; i>0, j<8; i--,j++ )
         {                  
                     availableMoves.push(Square.at(i, j));
         }
