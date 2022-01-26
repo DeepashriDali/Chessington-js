e6d809974986cbadf04172acb5f157700ce0102b
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-01-26 15:39:25 +0000

    n/a

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,31 +1,14 @@
 import Piece from './piece';
 import Square from '../square';
 import GameSettings from '../gameSettings';
+import Bishop from './bishop';
+import Rook from './rook';
 
-export default class Queen extends Piece {
+
+export default class Queen extends Rook, Bishop {
     constructor(player) {
         super(player);
     }
 
-    getAvailableMoves(board) {
-        let location = board.findPiece(this)
-        let availableMoves = [];
-        let boardLength = GameSettings.BOARD_SIZE;
-        /* for (let i = 0, j = 0; i < boardLength; i++, j++) {
-            if (i !== location.row && j !== location.col){
-            availableMoves.push(Square.at(i, j + 1));
-            }
-        } */
-        for (let i = 0; i < boardLength; i++) {
-            if (i !== location.col) {
-            availableMoves.push(Square.at(location.row, i));
-            }
-        }
-        for (let i = 0; i < boardLength; i++) {
-            if (i !== location.row) {
-            availableMoves.push(Square.at(i, location.col));
-            }
-        }
-        return availableMoves;
-    }
+    
 }
